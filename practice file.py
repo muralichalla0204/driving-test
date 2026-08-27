800bc4dcdf4e2e6ad5b122bd3ca35f38f357419d
Naresh5949 <padamalanaresh@gmail.com>
2026-08-27 17:41:05 +0530

    new programs add into the practice files

---
+++ practice file.py
@@ -0,0 +1,51 @@
+#m = "naresh"
+#print(m[::-1])
+
+#m = "naresh"
+#print(m[0:2]+m[2:][::-1])
+
+#m = "naresh padamala"
+#print(m.split()[::-1])
+
+m = "padamala"
+h =[]
+for i in m:
+    if i not in "aeiou":
+        h.append(i)
+print(h)
+
+m = "hello sri"
+g = []
+for i in m:
+    if i in "aeiou" and i not in g:
+        g.append(i)
+print(g)
+
+m = 123123
+d = []
+for i in str(m):
+    if str(m).count(i)>1 and i not in d:
+        d.append(i)
+print(d)
+
+m = 123
+p = 0
+for i in str(m):
+     p +=1
+print(p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
